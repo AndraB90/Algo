636e3f5f592cd3a9036b96dd0e7e88f7011e5fab
Andra <andra.botean@gmail.com>
2023-09-07 19:00:48 +0200

    added stopwatch class and tested bubblesort performance

---
+++ src/timer/StopWatch.java
@@ -0,0 +1,18 @@
+package timer;
+
+public class StopWatch {
+    private double startTime;
+    private double endTime;
+
+    public void start() {
+        startTime = System.currentTimeMillis();
+    }
+
+    public void stop() {
+        endTime = System.currentTimeMillis();
+    }
+
+    public void printElapsedTime() {
+        System.out.println("The total time for running was: " + (endTime - startTime) / 1000.0 + " seconds");
+    }
+}
